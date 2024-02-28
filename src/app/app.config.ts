a24342e829883d2458fe9d57ce9a58a17d082493
Ghodbane Tasnim <157584333+TasnimTest@users.noreply.github.com>
2024-02-29 00:21:16 +0100

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,30 +1,24 @@
-import { ApplicationConfig, NgModule } from '@angular/core';
+import { ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
 import { routes } from './app.routes';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { LayoutComponent } from './layout/layout.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-@NgModule({ // Add the missing @NgModule decorator
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-  ], // Remove NgModule from imports
-  providers: [provideRouter(routes), provideClientHydration()],
-  bootstrap: [AppComponent]
-})
-export class AppModule { // Define a class for the NgModule
-}
+import { withInMemoryScrolling } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideHttpClient(
+    withFetch(),
+    ),
+    provideRouter(routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      }),
+      ),
+    provideClientHydration(),
+   ]
 };
