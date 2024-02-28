a6775396853f1174c3033376e194ea3a557aea46
Ghodbane Tasnim <157584333+TasnimTest@users.noreply.github.com>
2024-02-29 00:21:50 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { RouterOutlet,RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
+  //hethi tji maktouba par defauts
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+ 
+  //na3mlou importation mta3 les pages eli 3ana
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    LoginComponent,
+    RouterModule,
+    LayoutComponent,
+    DashboardComponent
+  ],
+  //lahne n7otou lien mta3 app component html et css
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
+
+//hethi tji maktouba par defauts
 export class AppComponent {
   title = 'pfe-app';
 }
