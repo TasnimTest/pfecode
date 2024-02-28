73c7e9b2275a28a179ad2b2596f582fad649f799
Ghodbane Tasnim <157584333+TasnimTest@users.noreply.github.com>
2024-02-29 00:17:47 +0100

    Update layout.component.ts

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Route, Router, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  constructor(private router : Router){
+
+  }
+  //fi west url n7ot adresse api 
+  logoff(){
+    this.router.navigateByUrl('');
+  }
+
 
 }
